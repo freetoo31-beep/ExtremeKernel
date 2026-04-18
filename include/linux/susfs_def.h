4f489828837edac224ec8c9ef4ad34ac2af67a8c
freetoo31-beep <freetoo31@gmail.com>
2026-04-18 22:46:51 +0300

    Change DEFAULT_KSU_MNT_ID from 300000 to 10000

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -42,7 +42,7 @@
 #define TRY_UMOUNT_DEFAULT 0 /* used by susfs_try_umount() */
 #define TRY_UMOUNT_DETACH 1 /* used by susfs_try_umount() */
 
-#define DEFAULT_KSU_MNT_ID 300000 /* used by mount->mnt_id */
+#define DEFAULT_KSU_MNT_ID 10000 /* used by mount->mnt_id */
 #define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
 #define DEFAULT_KSU_MNT_GROUP_ID 5000 /* used by mount->mnt_group_id */
 #define VFSMOUNT_MNT_FLAGS_KSU_UNSHARED_MNT 0x80000000 /* used for mounts that are unshared by ksu process */
