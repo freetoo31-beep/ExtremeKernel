7df3cb81e6694a3c699f834a0e176723c3398526
freetoo31-beep <freetoo31@gmail.com>
2026-04-20 08:49:19 +0300

    Change susfs_init function return type to int

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -245,7 +245,8 @@ void susfs_get_enabled_features(void __user **user_info);
 void susfs_show_variant(void __user **user_info);
 void susfs_show_version(void __user **user_info);
 void susfs_start_sdcard_monitor_fn(void);
-void susfs_init(void);
+int susfs_init(void);
+
 
 #endif
 
